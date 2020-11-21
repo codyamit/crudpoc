6ee9354944c4321b6579c02426374334076caf5e
codyamit <amittonwork@gmail.com>
2020-11-21 15:02:45 +0530

    inital commit

--- crudpoc/src/App.js
+++ crudpoc/src/App.js
@@ -1,23 +1,157 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Button, FormControl, FormControlLabel, InputLabel, makeStyles, MenuItem, Select, Switch } from '@material-ui/core';
+import './App.scss';
+import AddIcon from '@material-ui/icons/Add';
 
 function App() {
+
+  const useStyles = makeStyles((theme) => ({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+  }));
+
+  const classes = useStyles();
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+   <div className="wrapper">
+     <h4>Add Labour</h4>
+     <div>    
+       <span>Employed</span>
+        <FormControlLabel
+          control={<Switch color="primary"  />}
+        />
+       <span>Contractual</span>
+      </div>
+
+      <div className="form vendor_form">
+        <h4 className="label">Vendor: </h4>
+        <FormControl className={classes.formControl}>
+        <InputLabel shrink id="demo-simple-select-placeholder-label-label">
+          Vendor (Labour) Name
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-placeholder-label-label"
+          id="demo-simple-select-placeholder-label"
+          value={age}
+          onChange={handleChange}
+          displayEmpty
+          className={classes.selectEmpty}
         >
-          Learn React
-        </a>
-      </header>
+          <MenuItem value="">
+            <em>Select</em>
+          </MenuItem>
+          <MenuItem value={10}>Ten</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel shrink id="demo-simple-select-placeholder-label-label">
+          State
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-placeholder-label-label"
+          id="demo-simple-select-placeholder-label"
+          value={age}
+          onChange={handleChange}
+          displayEmpty
+          className={classes.selectEmpty}
+        >
+          <MenuItem value="">
+            <em>Select</em>
+          </MenuItem>
+          <MenuItem value={10}>Ten</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel shrink id="demo-simple-select-placeholder-label-label">
+          Plant
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-placeholder-label-label"
+          id="demo-simple-select-placeholder-label"
+          value={age}
+          onChange={handleChange}
+          displayEmpty
+          className={classes.selectEmpty}
+        >
+          <MenuItem value="">
+            <em>Select</em>
+          </MenuItem>
+          <MenuItem value={10}>Ten</MenuItem>
+        </Select>
+      </FormControl>
+      </div>
+      
+      <div className="form vendor_form">
+        <h4 className="label">Rate Per Person: </h4>
+        <FormControl className={classes.formControl}>
+        <InputLabel shrink id="demo-simple-select-placeholder-label-label">
+          Machine Type
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-placeholder-label-label"
+          id="demo-simple-select-placeholder-label"
+          value={age}
+          onChange={handleChange}
+          displayEmpty
+          className={classes.selectEmpty}
+        >
+          <MenuItem value="">
+            <em>Select</em>
+          </MenuItem>
+          <MenuItem value={10}>Ten</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel shrink id="demo-simple-select-placeholder-label-label">
+          Labour Type
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-placeholder-label-label"
+          id="demo-simple-select-placeholder-label"
+          value={age}
+          onChange={handleChange}
+          displayEmpty
+          className={classes.selectEmpty}
+        >
+          <MenuItem value="">
+            <em>Select</em>
+          </MenuItem>
+          <MenuItem value={10}>Ten</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel shrink id="demo-simple-select-placeholder-label-label">
+          Rate Per Person/Annum (INR)
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-placeholder-label-label"
+          id="demo-simple-select-placeholder-label"
+          value={age}
+          onChange={handleChange}
+          displayEmpty
+          className={classes.selectEmpty}
+        >
+          <MenuItem value="">
+            <em>Select</em>
+          </MenuItem>
+          <MenuItem value={10}>Ten</MenuItem>
+        </Select>
+      </FormControl>
+      <Button variant="contained" color="primary"         startIcon={<AddIcon />}
+> Add</Button>
+      </div>
+   </div>
     </div>
   );
 }
